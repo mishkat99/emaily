4acfc5fd5fb1e5b18b35e44c94de40d786b4b252
mishkat99 <mishkat99@gmail.com>
2018-12-26 17:36:14 -0500

    Fixed keys.js error

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // decide what set of credentials to return
 
-if (process.ENV.NODE_NEV === 'production') {
+if (process.ENV.NODE_ENV === 'production') {
 	module.exports = require('./prod');
 } else {
 	module.exports = require('./dev');
