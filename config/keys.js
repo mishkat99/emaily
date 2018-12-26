e220e9597e2ebcbf8e56bb5c0b15284f6d7297af
mishkat99 <mishkat99@gmail.com>
2018-12-26 17:44:06 -0500

    Fixed type

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 // decide what set of credentials to return
 
-if (process.ENV.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	module.exports = require('./prod');
 } else {
 	module.exports = require('./dev');
